0afde14e4818372b848753fdcc6dc90a1a4dd128
Jasveer <kalsijasveer2003@gmail.com>
2024-07-09 14:19:25 +0530

    solved Problem5

--- Problem5_202217B3004/Problem5_202217B3004.c
+++ Problem5_202217B3004/Problem5_202217B3004.c
@@ -5,43 +5,44 @@
 
 void main()
 {
-	int queue[n], ch = 1, front = 0, rear = 0, i, j = 1, x = n;
+	int queue[n], ch, front = 0, rear = 0, count;
 	// clrscr();
 	printf("Queue using Array");
 	printf("\n1.Insertion \n2.Deletion \n3.Display \n4.Exit");
-	while (ch)
+	while (1)
 	{
 		printf("\nEnter the Choice:");
 		scanf("%d", &ch);
 		switch (ch)
 		{
 		case 1:
-			if (rear == x)
+			if (count == n)
 				printf("\n Queue is Full");
 			else
 			{
-				printf("\n Enter no %d:", j++);
+				printf("\n Enter no %d:", count + 1);
 				scanf("%d", &queue[rear++]);
+				count++;
 			}
 			break;
 		case 2:
-			if (front == rear)
+			if (count == 0)
 			{
 				printf("\n Queue is empty");
 			}
 			else
 			{
 				printf("\n Deleted Element is %d", queue[front++]);
-				x++;
+				count--;
 			}
 			break;
 		case 3:
+			printf("\n Queue Elements are:\n");
 			if (front == rear)
 				printf("\n Queue is Empty");
 			else
 			{
-				printf("\n Queue Elements are:\n");
-				for (i = front; i < rear; i++)
+				for (int i = front; i < rear; i++)
 				{
 					printf(" %d\n", queue[i]);
 				}
