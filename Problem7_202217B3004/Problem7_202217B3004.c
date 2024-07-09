d97926b608b48965affbc4ae39bd88f38905a87e
Jasveer <kalsijasveer2003@gmail.com>
2024-07-09 16:09:21 +0530

    solved Problem7

---
+++ Problem7_202217B3004/Problem7_202217B3004.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void bubble_sort(int arr[], int n)
+{
+	int i, j;
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+void print_array(int arr[], int n)
+{
+	printf("|");
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d", arr[i]);
+		if (i != n - 1)
+		{
+			printf("\t");
+		}
+	}
+	printf("|\n");
+}
+
+int main()
+{
+	int arr[] = {64, 34, 25, 12, 22, 11, 90};
+
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	printf("Original array: ");
+	print_array(arr, n);
+
+	bubble_sort(arr, n);
+	printf("Sorted array: ");
+	print_array(arr, n);
+
+	return 0;
+}
